17752a79e67468921c319c0f402a356812f8480d
Abilio Charles Chagas Soares <84188858+abilio-charles@users.noreply.github.com>
2023-10-30 21:48:58 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocs from './config/swagger'; // Se você criou a configuração do Swagger
-import appRoutes from './routes/app'; // Importando as rotas
-import { AppDataSource } from './data-source'; // Importando a inicialização do banco de dados
+import swaggerDocs from './config/swagger'; 
+import appRoutes from './routes/app'; 
+import { AppDataSource } from './data-source';
 
 const app = express();
 const port = 3000;
 
-// Middlewares
 app.use(express.json());
 
-// Configuração do Swagger (se você implementou)
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Usando as rotas definidas em app.ts
+
 app.use(appRoutes);
 
-// Inicialização do banco de dados e, em seguida, do servidor
+
 AppDataSource.initialize()
     .then(() => {
         console.log('Data Source has been initialized!');
